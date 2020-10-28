787b89117cc836010d566c3c3c3b226773073524
Chey Rose Flammer <cheyroseflammer@gmail.com>
2020-10-28 12:57:40 -0700

    blogful added

---
+++ blogful-scripts/seed.blogful.sql
@@ -0,0 +1,7 @@
+INSERT INTO blogful_articles (title, date_published, content) VALUES
+  ('Ovenbird', '4/18/2020', 'Seiurus aurocapillus');
+  ('Walrus', '4/08/2020', 'Serocapillus');
+  ('Platypus', '2/12/2020', 'Cornilus beachous');
+  ('Ostrich', '3/28/2020', 'Longbou johnues');
+  ('Giraffe', '7/07/2020', 'Neckobiguo ohnosolongi');
+  ('Zebra', '11/11/2020', 'Mounous stroups');
